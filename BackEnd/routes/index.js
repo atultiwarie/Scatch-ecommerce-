b7175ceab30a19f824454c75729808284b9511c6
atultiwarie <atultiwari4767@gmail.com>
2025-08-24 22:38:55 +0530

    cart ejs and routes

--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -10,15 +10,27 @@ router.get('/', (req, res) => {
 })
 
 router.get('/shop',isLoggedIn,async (req,res)=>{
-    let product = await productModel.find()
-    res.render('shop',{product})
+    let products = await productModel.find()
+    let success=req.flash("success")
+    res.render('shop',{products,success})
 })
 
-router.get('/addtocart/:id',isLoggedIn,async (req,res) => {
-    let user = await userModel.findOne({user: req.user.email})
+
+router.get('/cart',isLoggedIn, async (req,res) => {
+    let user = await userModel
+    .findOne({email:req.user.email})
+    .populate('cart')
+    const bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount)
+    res.render('cart',{user,bill})
+    
+})
+
+router.get('/addtocart/:productid',isLoggedIn,async (req,res) => {
+    let user = await userModel.findOne({email: req.user.email})
     user.cart.push(req.params.productid)
     await user.save()
-    
+    req.flash('success',"Product added to cart")
+    res.redirect('/shop')
 
 })
 
